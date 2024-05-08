453c1b2fade65e67eddb5d2089c7b822d24e2d5c
Rakesh <rs7937753@gmail.com>
2024-05-08 19:57:30 +0530

    Initial Commit

---
+++ LazyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazyDemo
+//
+//  Created by Rakesh Sharma on 08/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
